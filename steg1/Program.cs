8417e03175b12cc3968ec5864223c0fe8b1b5823
jasmine lindberg <Jasmine.lindberg@elev.ga.ntig.se>
2024-11-14 12:07:23 +0100

    first commit

---
+++ steg1/Program.cs
@@ -0,0 +1,45 @@
+﻿
+string gissasiffra;
+
+int siffra = 40;
+
+int svar;
+int rättsvar = Random.Shared.Next(1, 100);
+
+bool success;
+
+while (siffra == 40)
+{
+
+    Console.WriteLine("Gissa ett tal mellan 1-100");
+    gissasiffra = Console.ReadLine();
+    success = int.TryParse(gissasiffra, out svar);
+
+
+    if (svar < rättsvar)
+    {
+        Console.WriteLine("För lågt");
+    }
+    else if (svar > rättsvar)
+    {
+        Console.WriteLine("För högt");
+    }
+    else if (svar == rättsvar)
+    {
+        Console.WriteLine("rätt");
+        siffra++;
+    }
+
+}
+
+Console.ReadLine();
+
+Console.WriteLine($" rätt svar var {rättsvar} grattis");
+Console.ReadLine();
+
+
+
+
+
+
+
